8499537777d96790e1165f525270b058aab697b3
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-12-03 01:08:36 +0100

    Built Cards section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import {Container} from './components/styles/Container.styled'
 import Header from './components/Header';
+import Card from './components/Card';
 import GlobalStyles from './components/styles/Global';
+import content from './content'
 
 const theme = {
   colors: {
@@ -19,7 +21,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello world</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
       </>
     </ThemeProvider>
