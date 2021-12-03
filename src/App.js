dd98187e28b68864fb4045a54ba606f37f1140fd
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-12-03 01:36:31 +0100

    Built Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import {Container} from './components/styles/Container.styled'
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Card from './components/Card';
 import GlobalStyles from './components/styles/Global';
 import content from './content'
@@ -25,6 +26,7 @@ function App() {
             <Card key={index} item={item} />
           ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
